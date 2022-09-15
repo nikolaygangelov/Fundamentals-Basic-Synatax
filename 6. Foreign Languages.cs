e5c4fd6fa368fe70028c94fa2d42937925cb3274
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-15 23:28:39 +0300

    Add files via upload

---
+++ 6. Foreign Languages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6._Foreign_Languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            if (name == "England" || name == "USA")
+            {
+                Console.WriteLine("English");
+            }else if (name == "Spain" || name == "Argentina" || name == "Mexico")
+            {
+                Console.WriteLine("Spanish");
+            }
+            else { Console.WriteLine("unknown"); }
+        }
+    }
+}
