5c88926f5aa1ce5707044c4121829c3946f58e71
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-15 23:09:44 +0300

    Add files via upload

---
+++ 4. Back in 30 Minutes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4._Back_in_30_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine()) + 30;
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
