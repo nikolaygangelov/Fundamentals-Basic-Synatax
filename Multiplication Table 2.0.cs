0d1ba1471ce79458813b804eb8fda0fa7a14f265
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2022-09-17 13:54:28 +0300

    Add files via upload

---
+++ Multiplication Table 2.0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _11._Multiplication_Table_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            int product = input * multiplier;
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{input} X {multiplier} = {product}");
+            }
+            else
+            {
+                while (multiplier <= 10)
+                {
+                    Console.WriteLine($"{input} X {multiplier} = {input * multiplier}");
+                    multiplier++;
+                }
+            }
+        }
+    }
+}
